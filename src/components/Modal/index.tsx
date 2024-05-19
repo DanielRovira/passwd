3e13c92eb38e3593cfc6b679e3df2617906ab78a
Daniel Rovira <danielroviratorres@gmail.com>
2024-05-19 18:30:56 -0300

    Init

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Pressable } from "react-native";
+
+
+export default function Modal() {
+
+    return(
+        <View>
+            <Text>Oi</Text>
+        </View>
+    );
+}
